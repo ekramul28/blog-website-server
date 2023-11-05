daf85ed1d658e155c649bc6710fb56e03fb38180
Ekramul <mdekramulhassan168@gmail.com>
2023-11-05 22:07:54 +0600

    delet api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -51,6 +51,15 @@ async function run() {
             }
             const cursor = await allWishlist.find(query).toArray()
             res.send(cursor)
+        })
+        // app.get('/wishlist/:id',async(req,res=>{
+
+        // }))
+        app.delete('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await allWishlist.deleteOne(query);
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
